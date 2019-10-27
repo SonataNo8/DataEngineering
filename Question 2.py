10068a243f1e6d1094fa8be575b6e89c84e09699
Joey Anderson <joanderson@peoplenetonline.com>
2019-10-27 11:22:06 -0500

    Create Question 2.py

---
+++ Question 2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data=pd.read_csv("Congress_White_House.csv")
+
+#Let's target Employment status and salary
+cols_to_drop=['Employee Name','Pay Basis','Position Title']
+
+data.drop(columns=cols_to_drop,inplace=True)
+data_status=pd.get_dummies(data['Employee Status'])
+data_concat=pd.concat([data, data_status],axis=1)
+data_concat.drop(columns='Employee Status',inplace=True)
